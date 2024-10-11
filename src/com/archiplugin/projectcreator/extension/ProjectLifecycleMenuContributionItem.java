cf3a704fc7e4229cff7a1bb86b223f0a3feb625c
Merenptah <8060706+Merenptah@users.noreply.github.com>
2024-10-11 08:07:45 +0200

    Use a hierarchical menu structure for better usability

--- src/com/archiplugin/projectcreator/extension/ProjectLifecycleMenuContributionItem.java
+++ src/com/archiplugin/projectcreator/extension/ProjectLifecycleMenuContributionItem.java
@@ -7,15 +7,16 @@ import java.util.stream.Collectors;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.commands.CommandStack;
 import org.eclipse.jface.action.Action;
-import org.eclipse.jface.action.ActionContributionItem;
-import org.eclipse.jface.action.Separator;
-import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.action.ContributionItem;
+import org.eclipse.jface.action.IContributionItem;
+import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Menu;
 import org.eclipse.ui.ISelectionService;
-import org.eclipse.ui.menus.ExtensionContributionFactory;
-import org.eclipse.ui.menus.IContributionRoot;
+import org.eclipse.ui.menus.IWorkbenchContribution;
 import org.eclipse.ui.services.IServiceLocator;
 
+import com.archimatetool.editor.views.tree.ITreeModelView;
 import com.archimatetool.model.FolderType;
 import com.archimatetool.model.IFolder;
 import com.archimatetool.model.IProperties;
@@ -24,30 +25,52 @@ import com.archiplugin.projectcreator.preferences.Preferences;
 import com.archiplugin.projectcreator.project.lifecycle.MandatoryPropertiesDefinition;
 import com.archiplugin.projectcreator.project.lifecycle.MoveProject;
 
-public class ProjectLifecycleMenuExtensionContributionFactory extends ExtensionContributionFactory {
+public class ProjectLifecycleMenuContributionItem extends ContributionItem implements IWorkbenchContribution {
 
-	public ProjectLifecycleMenuExtensionContributionFactory() {
+	private MenuManager menuManager;
+
+	private ISelectionService selectionService;
+
+	@Override
+	public void initialize(IServiceLocator serviceLocator) {
+		this.selectionService = serviceLocator.getService(ISelectionService.class);
 	}
 
 	@Override
-	public void createContributionItems(IServiceLocator serviceLocator, IContributionRoot additions) {
-		var selectionService = serviceLocator.getService(ISelectionService.class);
-		var selection = (IStructuredSelection) selectionService.getSelection();
+	public void fill(Menu menu, int index) {
+		if (menuManager != null) {
+			menuManager.dispose();
+		}
+
+		IStructuredSelection selection = getCurrentSelection();
+		if (selection == null) {
+			return;
+		}
+
+		menuManager = new MenuManager();
 
 		currentFolder(selection).ifPresent(currentFolder -> {
 			var matchingLifecycles = Preferences.getPreferenceLifecycles().toLifecycles()
 					.findMatchingLifecycles(currentFolder);
 
 			matchingLifecycles.forEach(lc -> {
-				additions.addContributionItem(new Separator(), null);
-
-				var moveProjectToNextStage = new ActionContributionItem(
-						new MoveProjectToNextStageAction(lc, currentFolder));
-				additions.addContributionItem(moveProjectToNextStage, null);
+				var moveProjectToNextStage = new MoveProjectToNextStageAction(lc, currentFolder);
+				menuManager.add(moveProjectToNextStage);
 			});
-
 		});
 
+		for (IContributionItem item : menuManager.getItems()) {
+			item.fill(menu, index++);
+		}
+	}
+
+	@Override
+	public boolean isDynamic() {
+		return true;
+	}
+
+	private IStructuredSelection getCurrentSelection() {
+		return (IStructuredSelection) selectionService.getSelection(ITreeModelView.ID);
 	}
 
 	private class MoveProjectToNextStageAction extends Action {
@@ -61,7 +84,7 @@ public class ProjectLifecycleMenuExtensionContributionFactory extends ExtensionC
 
 		@Override
 		public String getText() {
-			return Messages.MoveProjectToNextStageMenuEntry + " " + lifecycleDefinition.getToFolderName();
+			return lifecycleDefinition.getToFolderName();
 		}
 
 		@Override
@@ -77,11 +100,6 @@ public class ProjectLifecycleMenuExtensionContributionFactory extends ExtensionC
 		public String getId() {
 			return "moveProjectToNextStageAction"; //$NON-NLS-1$
 		};
-
-		@Override
-		public ImageDescriptor getImageDescriptor() {
-			return Images.projectImage();
-		}
 	};
 
 	private Optional<IFolder> currentFolder(IStructuredSelection selection) {
